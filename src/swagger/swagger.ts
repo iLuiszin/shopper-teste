9a69b2b505a8d14e71c08ef13b5cf755c7925992
Luis Felipe <oluis10@gmail.com>
2024-08-30 21:49:20 -0300

    feat: adding Model Measure

--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -19,33 +19,39 @@ async function generateSwaggerDocs() {
         description: 'Servidor localhost.',
       },
     ],
-    definitions: {
-      UploadRequest: {
-        image: 'string',
-        customer_code: 'string',
-        measure_datetime: 'datetime',
-        measure_type: 'string',
-      },
-      UploadResponse: {
-        image_url: 'string',
-        measure_value: 'number',
-        measure_uuid: 'string',
-      },
-      ConfirmRequest: {
-        measure_uuid: 'string',
-        confirmed_value: 'number',
-      },
-      ListMeasuresResponse: {
-        customer_code: 'string',
-        measures: [
-          {
-            measure_uuid: 'string',
-            measure_datetime: 'datetime',
-            measure_type: 'string',
-            has_confirmed: 'boolean',
-            image_url: 'string',
+    '@definitions': {
+      Measure: {
+        type: 'object',
+        properties: {
+          measure_uuid: {
+            type: 'string',
+            example: '4bbb02d-14c1-4a64-bc9f-58fb40269c3e',
           },
-        ],
+          measure_value: {
+            type: 'number',
+            example: 10,
+          },
+          customer_code: {
+            type: 'string',
+            example: 'CUST1234',
+          },
+          measure_type: {
+            type: 'string',
+            example: 'WATER',
+          },
+          measure_datetime: {
+            type: 'string',
+            example: '2024-08-30T14:00:00Z',
+          },
+          image_url: {
+            type: 'string',
+            example: 'http://example.com/image_url',
+          },
+          has_confirmed: {
+            type: 'boolean',
+            example: true,
+          },
+        },
       },
     },
     consumes: ['application/json'],
